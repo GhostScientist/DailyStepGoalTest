146e3b3a05b2207136509a7cbcb96b13426d1cc5
Dakota Kim <codingvampire@gmail.com>
2023-09-21 20:24:50 -0400

    Initial Commit

---
+++ DailyStepGoalTest Watch App/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  DailyStepGoalTest Watch App
+//
+//  Created by Dakota Kim on 9/21/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ DailyStepGoalTest Watch App/DailyStepGoalTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  DailyStepGoalTestApp.swift
+//  DailyStepGoalTest Watch App
+//
+//  Created by Dakota Kim on 9/21/23.
+//
+
+import SwiftUI
+
+@main
+struct DailyStepGoalTest_Watch_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
